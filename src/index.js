b37e336112a44109a4cc78a31de20419adad04f2
doonee <d2607@naver.com>
2023-03-17 15:50:09 +0900

    auth with previous backend project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/**
+ * 동영상 강좌
+ * https://www.youtube.com/watch?v=brcHK3P6ChQ&list=PL0Zuz27SZ-6PrE9srvEn8nbhOOyxnWXfp&index=33
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
